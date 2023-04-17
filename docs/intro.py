355cfce03983144e1cdb6fc9edf4cdfc2fc97a62
King13324 <130067755+King13324@users.noreply.github.com>
2023-04-17 18:23:57 +0000

    commit admins/admin_accounts commit docs/intro

---
+++ docs/intro.py
@@ -0,0 +1,7 @@
+doctext = """
+
+        Hello! It seems this is your first time using this terminal. If you need help using it,
+        please refer to the terminal documentation. Otherwise, if you need assistance:
+
+                                        Leave.
+"""
